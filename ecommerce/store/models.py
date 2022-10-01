3844e86634322c87eab1449eb842e8cbbf6ccaa5
Correa7 <correadamian2019@gmail.com>
2022-10-01 17:29:12 -0300

    Views, enlace de modelos con views y html

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -24,7 +24,7 @@ class Products (models.Model):
     name = models.CharField (max_length = 200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False) ##por si el producto que vendemos es o no digital###
-    #image#
+    image = models.ImageField (null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -37,17 +37,35 @@ class Order (models.Model):
     date_ordered = models.DateTimeField (auto_now_add =True)
     complete = models.BooleanField(default=False, null=True, blank= False)       #### instancia de comprobacion de si la orden finalizo, o se puede seguir agregando productos####
     transaction_id = models.CharField (max_length = 200, null=True)
+
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem.all()
+        total = sum ([item.get_total for item in orderitems ])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem.all()
+        total = sum ([item.quantity for item in orderitems ])
+        return total
 
 ##### OrderItems ####
 
 class OrderItems (models.Model):
     product = models.ForeignKey(Products, on_delete= models.SET_NULL, null=True, blank=True)
-    order = models.ForeignKey(Order, on_delete= models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete= models.SET_NULL, null=True, blank=True, related_name='orderitem')
     quantity = models.IntegerField(default=0, null=True, blank= True)
     date_added= models.DateTimeField (auto_now_add =True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
 
 
 ##### ShippingAddress (envios)#####
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import *
+
+# from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 
 
@@ -9,12 +13,37 @@ def store(request):
      return render(request, 'store/store.html', context)
 
 
+
+# @login_required
 def cart(request):
      
-     context = {}
+     if request.user.is_authenticated:        ## una manera de verificar si esta logueado otra seria con el decorador @login_required##
+          customer = request.user.customer
+
+          order, created = Order.objects.get_or_create( customer=customer, complete=False)   # .get_or_create() estmos consultando si el cliente tiene un pedido y/o lo creamos#
+                                   # pasamos al cliente com parametro, y complete=False por que el pedido esta abierto aun##
+                         
+          items = order.orderitem.all() #de lo anterior recuperamos el pedido y aqui sus items, order.orderitem.all() orderitem es el related_name que le pusimos##
+
+     else:
+          items = []
+          order = {'get_cart_total':0 , 'get_cart_item':0} #se crea un carro vacio por si no esta logueado el usuario#
+
+     context = {'items': items,'order': order }
      return render(request, 'store/cart.html', context)
      
 
 def checkout(request):
-      context = {}
-      return render(request, 'store/checkout.html', context)
+
+     if request.user.is_authenticated:        #mismo que en def cart #####
+
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create( customer=customer, complete=False)             
+          items = order.orderitem.all() 
+
+     else:
+          items = []
+          order = {'get_cart_total':0 , 'get_cart_item':0}
+
+     context = {'items': items,'order': order }
+     return render(request, 'store/checkout.html', context)
